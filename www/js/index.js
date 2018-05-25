8c3a6df95033cb1fe0b3d01a2daa8fd7d29e5d08
taistril <mail@royellwood.com>
2018-05-25 11:22:35 +0100

    first commit

--- www/js/index.js
+++ www/js/index.js
@@ -7,7 +7,7 @@ function onDeviceReady() {
 function playMP3() {
     // var mp3URL = getMediaURL("sounds/button-1.mp3");
 
-        alert('PlayMP3');
+     //   alert('PlayMP3');
         
         
     var media = new Media("/android_asset/www/sounds/button-1.mp3", mediaOK, mediaError);
