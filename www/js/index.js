55f09106184e1c46dcc773d818038f8f50b3f479
taistril <mail@royellwood.com>
2018-05-29 11:43:23 +0100

    first commit

--- www/js/index.js
+++ www/js/index.js
@@ -1,68 +1,24 @@
-// src="https://jsconsole.com/js/remote.js?aaaaa";
-
 document.addEventListener('deviceready', onDeviceReady, false);
 
-
-
 function onDeviceReady() {
-
-//        src="http://jsconsole.com/js/remote.js?cdaa1f08-24a1-4507-8ff8-d7e1f3d7c7d3";
-
 	document.querySelector("#playMp3").addEventListener("touchend", playMP3, false);
 };
 
 function playMP3() {
+    var mp3URL = getMediaURL("sounds/button-1.mp3");
+    var media = new Media(mp3URL, null, mediaError);
+    media.play();
+}
 
+function getMediaURL(s) {
+    if(device.platform.toLowerCase() === "android") return "/android_asset/www/" + s;
+    return s;
+}
 
-    // var mp3URL = getMediaURL("sounds/button-1.mp3");
-
-        alert('PlayMP3');
-        
-    //     var roy = "aaaaaaaaaaaaaaaa";
-        
-    //     document.write(roy);
-
-       var media = new Media(/android_asset/www/sounds/button-1.mp3");
-
-    // var media = new Media("http://cbpsyorks.co.uk/mp3/A1.mp3");
-   
-   
-   // document.write(media);
-    
-
-        alert('PlayMP3-B');
-        
-        
-     //    var royb = "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB";
-        
-     //    document.write(royb);
-
-    
-
-    
-    
-
-         
-        
-  /*      
-    //  var media = new Media("http://cbpsyorks.co.uk/mp3/A1.mp3", 
-    
-     //   var media = new Media(/android_asset/www/sounds/button-1.mp3",
-    
-    var media = new Media("http://cbpsyorks.co.uk/mp3/A1.mp3", 
-    
-            // success callback
-            function() {
-                console.log("playAudio():Audio Success");
-            },
-            // error callback
-            function(err) {
-                console.log("playAudio():Audio Error: "+err);
-    }
-    
-    
-    
-    
+function mediaError(e) {
+    alert('Media Error');
+    alert(JSON.stringify(e));
+}
     
     
 //    mediaOK, mediaError);
